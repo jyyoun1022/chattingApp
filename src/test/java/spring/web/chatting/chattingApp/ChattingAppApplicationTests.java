dbb44909c31be3632acaa38ba230e3e0eed47a89
jyyoun1022 <96053246+jyyoun1022@users.noreply.github.com>
2023-03-28 13:35:43 +0900

    Initial commit

---
+++ src/test/java/spring/web/chatting/chattingApp/ChattingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.web.chatting.chattingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
